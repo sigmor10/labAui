d818dc89e3d42eaa8f9bc3b5cf88909d6aca6771
sigmor10 <sigmor10@wp.pl>
2025-01-19 01:55:06 +0100

    finished db migration and added config server service

---
+++ MultiApp/ConfigApp/src/test/java/lab7/configapp/ConfigAppApplicationTests.java
@@ -0,0 +1,13 @@
+package lab7.configapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
